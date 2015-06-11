d81a5635e080fe8381804aff5e5c1de8fab5a459
pmucha <peter.mucha@affinitas.de>
2015-06-11 13:45:51 +0200

    refactorings

--- feign-request.js
+++ feign-request.js
@@ -1,30 +1,29 @@
 
 var Args = require('args-js');
 var request = require('request');
-function createRequestJsOptions(baseUrl, requestOptions, parameters){
-	var options  = Args([
-			{ method: Args.STRING | Args.Optional, _default: 'GET' },
-			{ uri: Args.STRING | Args.Required}
-		], [requestOptions]);
-	options.qs = parameters;
-	options.baseUrl = baseUrl;
-	return options;
-}
+
+
+
 
 
 function FeignRequestClient(){
 	var args  = Args([
-			{ request: Args.FUNCTION | Args.Optional }
+			{ request: Args.FUNCTION | Args.Optional, _default: request},
+			{ debug: Args.BOOL | Args.Optional, _default: false},
+			{ json:  Args.BOOL | Args.Optional, _default: true}
 		], arguments);
 	
 	this.requestFn = args.request;
-	if (this.requestFn == null){
-		this.requestFn = request;
-	};
+	this.requestFn.debug = args.debug;
+	this.isJson = args.json;
+	
+	if (args.json){
+		this.requestFn = this.requestFn.defaults({json: true});
+	}
 }
 
 FeignRequestClient.prototype.request =  function(request){
-	var options = createRequestJsOptions(request.baseUrl, request.options, request.parameters);
+	var options = this._createRequestJsOptions(request.baseUrl, request.options, request.parameters);
 	var _this = this;
 	var promise = new Promise(function(resolve, reject){
 		_this.requestFn(options, function(error, response, body){
@@ -36,6 +35,24 @@ FeignRequestClient.prototype.request =  function(request){
 	return promise;
 };
 
+FeignRequestClient.prototype._createRequestJsOptions = function(baseUrl, requestOptions, parameters){
+	var options  = Args([
+			{ method: Args.STRING | Args.Optional, _default: 'GET' },
+			{ uri: Args.STRING | Args.Required}
+		], [requestOptions]);
+
+			
+	if (options.method == 'GET'){
+		options.qs = parameters;
+	} else if (this.isJson) {
+		options.body = parameters;
+	} else {
+		options.form = parameters;
+	}
+	
+	options.baseUrl = baseUrl;
+	return options;
+}
 
 
 module.exports = FeignRequestClient;
